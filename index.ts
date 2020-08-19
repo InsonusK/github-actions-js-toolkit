ed43c905be1a6e8ef831255ba6a5965a3253a658
InsonusK <insonus.k@gmail.com>
2020-08-20 02:53:43 +0300

    add core.info element

--- index.ts
+++ index.ts
@@ -16,6 +16,7 @@ async function run(): Promise<void> {
     });
     core.info(`-- octokit.repos.listReleases: length = ${repoList.data.length}`);
     repoList.data.forEach((element) => {
+        core.info(`--- element:${element}`);
         core.info(`--- element.id:${element.id}`);
         core.info(`--- element.name:${element.name}`);
         core.info(`--- element.draft:${element.draft}`);
